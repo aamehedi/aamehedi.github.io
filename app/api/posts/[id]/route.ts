ba869e29932a29c514cf664eeaf9d168000caa1e
aamehedi <whitepaper321@gmail.com>
2025-05-28 12:55:37 +0600

    Fixing Type Error any 2

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,7 +1,7 @@
 import {readFile} from "@/lib/util";
 import path from "path";
 
-export const GET = async (request, { params }) => {
+export const GET = async (request : any, { params } : { params: any}) => {
     try {
         const fileData = await readFile(path.join(process.cwd(), 'posts', params.id));
         return new Response(JSON.stringify(fileData), {
